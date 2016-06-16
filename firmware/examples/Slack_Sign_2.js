f8be7f92adccd9f5b677125128187117ef7ad867
telliottosceola <travis@controlanything.com>
2016-06-16 14:31:08 -0500

    Added JS files for integration into Hook.io

---
+++ firmware/examples/Slack_Sign_2.js
@@ -0,0 +1,32 @@
+module['exports'] = function echoHttp (hook) {
+  post('https://api.particle.io/v1/devices/#ENTER_DEVICE_ID_HERE#controlRelay',{arg : '2flicker', access_token : 'ENTER_YOUR_PARTICLE_ACCOUNT_ACCESS_TOKEN'})
+};
+
+function post(path, params, method) {
+  	var querystring = require('querystring');
+  	var https = require('https');
+  
+  	var post_data = querystring.stringify(params);
+  	var post_options = {
+      host: 'api.particle.io',
+      port: '443',
+      path: '/v1/devices/#ENTER_DEVICE_ID_HERE#/controlRelay',
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+      }
+  };
+
+  // Set up the request
+  var post_req = https.request(post_options, function(res) {
+      res.setEncoding('utf8');
+      res.on('data', function (chunk) {
+          //console.log('Response: ' + chunk);
+      });
+  });
+
+  // post the data
+  
+  post_req.write(post_data);
+  post_req.end();
+}
